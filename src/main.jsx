fc78f50f22b7dbe34b5ff5db8285dcdcd4ee3e8a
Amal <99679990+amalsujabiju5@users.noreply.github.com>
2024-10-05 13:03:20 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Root from "./root";
+import Root from "./Root";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
